8411b5177dde76a654baecae326f8f5c51044219
KeeganBruer <keegan.bruer@gmail.com>
2018-01-23 17:14:25 -0500

    New Encoder Setup

--- src/com/team5854/utils/driveSystem/DriveSystem.java
+++ src/com/team5854/utils/driveSystem/DriveSystem.java
@@ -1,6 +1,7 @@
 package com.team5854.utils.driveSystem;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 public class DriveSystem {
 	private TalonSRX leftMotor;
@@ -14,6 +15,7 @@ public class DriveSystem {
 		//-----SET THE MASTER MOTORS----
 		this.leftMotor = left;
 		this.rightMotor = right;
+		setupAutonomous();
 	}
 	
 	public DriveSystem(int type, TalonSRX frontLeft, TalonSRX frontRight, TalonSRX backLeft, TalonSRX backRight) {
@@ -31,6 +33,7 @@ public class DriveSystem {
 			this.backLeftMotor = backLeft;
 			this.backRightMotor = backRight;
 		}
+		setupAutonomous();
 	}
 	
 	public DriveSystem(int type, TalonSRX frontLeft, TalonSRX frontRight, TalonSRX backLeft, TalonSRX backRight,
@@ -65,6 +68,7 @@ public class DriveSystem {
 			backLeftTwo.set(ControlMode.Follower, this.backLeftMotor.getDeviceID());
 			backRightTwo.set(ControlMode.Follower, this.backRightMotor.getDeviceID());
 		}
+		setupAutonomous();
 	}
 	
 	
@@ -86,6 +90,84 @@ public class DriveSystem {
 			System.err.println("DRIVESYTEM: Drive Type Incorrent For A \"x y tist rotate\" Drive Type.");
 		}
 	}
+	public void drive(double position) {
+		leftMotor.set(ControlMode.Position, position);
+		rightMotor.set(ControlMode.Position, position);
+		if (backLeftMotor != null && backRightMotor != null) {
+			backLeftMotor.set(ControlMode.Position, position);
+			backRightMotor.set(ControlMode.Position, position);
+		}
+	}
+	
+	private void setupAutonomous() {
+		int absolutePosition = this.leftMotor.getSelectedSensorPosition(10) & 0xFFF;
+ 		this.leftMotor.setSelectedSensorPosition(absolutePosition, 0, 10);
+ 		this.rightMotor.setSelectedSensorPosition(absolutePosition, 0, 10);
+	 	/* choose the sensor and sensor direction */
+ 		this.leftMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+ 		this.rightMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+ 		this.leftMotor.setSensorPhase(true);
+ 		this.rightMotor.setSensorPhase(true);
+ 		/* set the peak and nominal outputs, 12V means full */
+ 		this.leftMotor.configNominalOutputForward(0, 10);
+ 		this.rightMotor.configNominalOutputForward(0, 10);
+ 		this.leftMotor.configNominalOutputReverse(0, 10);
+ 		this.rightMotor.configNominalOutputReverse(0, 10);
+ 		this.leftMotor.configPeakOutputForward(1, 10);
+ 		this.rightMotor.configPeakOutputForward(1, 10);
+ 		this.leftMotor.configPeakOutputReverse(-1, 10);
+ 		this.rightMotor.configPeakOutputReverse(-1, 10);
+ 		/* set the allowable closed-loop error,
+ 		 * Closed-Loop output will be neutral within this range.
+ 		 * See Table in Section 17.2.1 for native units per rotation. 
+ 		 */
+ 		this.leftMotor.configAllowableClosedloopError(0, 0, 10); /* always servo */
+ 		this.rightMotor.configAllowableClosedloopError(0, 0, 10); /* always servo */
+ 		/* set closed loop gains in slot0 */
+ 		this.leftMotor.config_kF(0, 0.0, 10);
+ 		this.rightMotor.config_kF(0, 0.0, 10);
+ 		this.leftMotor.config_kP(0, 0.1, 10);
+ 		this.rightMotor.config_kP(0, 0.1, 10);
+ 		this.leftMotor.config_kI(0, 0.0, 10);
+ 		this.rightMotor.config_kI(0, 0.0, 10);
+ 		this.leftMotor.config_kD(0, 0.0, 10);
+ 		this.rightMotor.config_kD(0, 0.0, 10);
+ 		
+ 		if (backLeftMotor != null && backRightMotor != null) {
+ 			this.backLeftMotor.setSelectedSensorPosition(absolutePosition, 0, 10);
+ 			this.backRightMotor.setSelectedSensorPosition(absolutePosition, 0, 10);
+ 			/* choose the sensor and sensor direction */
+ 			this.backLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+ 			this.backRightMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+ 			this.backLeftMotor.setSensorPhase(true);
+ 			this.backRightMotor.setSensorPhase(true);
+ 			/* set the peak and nominal outputs, 12V means full */
+ 			this.backLeftMotor.configNominalOutputForward(0, 10);
+ 			this.backRightMotor.configNominalOutputForward(0, 10);
+ 			this.backLeftMotor.configNominalOutputReverse(0, 10);
+ 			this.backRightMotor.configNominalOutputReverse(0, 10);
+ 			this.backLeftMotor.configPeakOutputForward(1, 10);
+ 			this.backRightMotor.configPeakOutputForward(1, 10);
+ 			this.backLeftMotor.configPeakOutputReverse(-1, 10);
+ 			this.backRightMotor.configPeakOutputReverse(-1, 10);
+ 			/* set the allowable closed-loop error,
+ 			 * Closed-Loop output will be neutral within this range.
+ 			 * See Table in Section 17.2.1 for native units per rotation. 
+ 			 */
+ 			this.backLeftMotor.configAllowableClosedloopError(0, 0, 10); /* always servo */
+ 			this.backRightMotor.configAllowableClosedloopError(0, 0, 10); /* always servo */
+ 			/* set closed loop gains in slot0 */
+ 			this.backLeftMotor.config_kF(0, 0.0, 10);
+ 			this.backRightMotor.config_kF(0, 0.0, 10);
+ 			this.backLeftMotor.config_kP(0, 0.1, 10);
+ 			this.backRightMotor.config_kP(0, 0.1, 10);
+ 			this.backLeftMotor.config_kI(0, 0.0, 10);
+ 			this.backRightMotor.config_kI(0, 0.0, 10);
+ 			this.backLeftMotor.config_kD(0, 0.0, 10);
+ 			this.backRightMotor.config_kD(0, 0.0, 10);	
+ 		}
+	}
+
 	public class DriveType {
 		public static final int TankDrive = 2343;
 		public static final int ArcadeDrive = 2443;
