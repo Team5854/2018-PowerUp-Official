14dbf684c13351f78ce40aed5d407b3a14000969
ebolakitten <vera.winebarger1@gmail.com>
2018-02-03 08:33:03 -0800

    Committing the Grabber Code

---
+++ src/com/team5854/utils/mechanism/Grabber.java
@@ -0,0 +1,26 @@
+package com.team5854.utils.mechanism;
+
+import edu.wpi.first.wpilibj.VictorSP;
+
+public class Grabber {
+	private VictorSP left;
+	private VictorSP right;
+	public Grabber(VictorSP left, VictorSP right) {
+		this.left = left;
+		this.right = right;
+	}
+	/**
+	 * Intake mode
+	 */
+	public void intake() {
+		this.left.set(-1);
+		this.right.set(1);
+	}
+	/**
+	 * Output mode
+	 */
+	public void output() {
+		this.left.set(1);
+		this.right.set(-1);
+	}
+}
